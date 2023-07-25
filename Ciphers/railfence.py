0cb8d2924d15dd03cffc28286ead48d4a486a1dc
root <92035508+AjayBadrinath@users.noreply.github.com>
2023-07-26 01:38:18 +0530

    commit railfence

---
+++ Ciphers/railfence.py
@@ -0,0 +1,44 @@
+def railfence(st,key):
+    l=2*key-2
+    j=0
+    s1=""
+    m=0
+    for i in range(key):
+        m=i
+        s1+=st[m]
+        while m<len(st):
+            print(m)
+            try:
+                #s1+=st[m]
+                                if(l==0):
+                                    m+=j
+                                    s1+=st[m]
+                                    continue
+                                m+=l
+                                s1+=st[m]
+                                if(j==0):
+                                    m+=l
+                                    s1+=st[m]
+                                    continue
+                                m+=j
+                                s1+=st[m]
+            except Exception:
+                break
+        j+=2
+        l-=2
+    #k=list(s1)
+    #k.pop(1)
+    #k.pop(-1)
+    #k.pop(2)
+    #k.pop(-2)
+    #print(k,s1)
+    return s1
+x1=railfence("wearediscoveredrunatonce",4)
+i=0
+#crappy bruteforcing
+while x1!="wearediscoveredrunatonce":
+    x1=railfence(x1,4)
+    print(x1)
+    print(i)
+    i+=1
+
