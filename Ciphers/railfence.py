eb944fb1a3a70650916dda19ecf897c80b5fd530
root <92035508+AjayBadrinath@users.noreply.github.com>
2023-07-27 00:50:58 +0530

    Commit decrypt function

--- Ciphers/railfence.py
+++ Ciphers/railfence.py
@@ -26,19 +26,70 @@ def railfence(st,key):
                 break
         j+=2
         l-=2
-    #k=list(s1)
-    #k.pop(1)
-    #k.pop(-1)
-    #k.pop(2)
-    #k.pop(-2)
-    #print(k,s1)
+    
     return s1
-x1=railfence("wearediscoveredrunatonce",4)
+x1=railfence("theyareattackingfromthenorth",4)
 i=0
+print(x1)
+'''
 #crappy bruteforcing
-while x1!="wearediscoveredrunatonce":
+while x1!="defendtheeastwall":
     x1=railfence(x1,4)
     print(x1)
     print(i)
     i+=1
+    '''
 
+def decrypt(stt_cipher,key):
+    l1=["" for i in range(len(stt_cipher))]
+    # 0-len(x)  2-len-2 4 - len-k
+    l=2*(key-1)
+    j=0
+    m=0
+    i=0
+    ol_val=0
+    for w in range(key): 
+        m=ol_val
+        l1[m]=stt_cipher[i]
+        i+=1
+        while m<len(stt_cipher):
+            #print(stt_cipher[i])
+            try:
+                #s1+=st[m]
+                                if(l==0):
+                                    m+=j
+                                    l1[m]=stt_cipher[i]
+                                    i+=1
+                                    continue
+                                if(j==0):
+                                    m+=l
+                                    l1[m]=stt_cipher[i]
+                                    i+=1
+                                    continue
+                                m+=l
+                                l1[m]=stt_cipher[i]
+                                i+=1
+                                
+                                m+=j
+                                l1[m]+=stt_cipher[i]
+                                i+=1
+                                
+                                
+            except Exception:
+                break
+        j+=2
+        ol_val+=1
+        l-=2
+    s=""
+    s=s.join(l1)
+    print(s)
+
+#decrypt("dnetleedheswlftaa",3)    
+decrypt("tekoohracirmnreatanftetytghh",4)         
+
+              
+
+
+
+
+    
