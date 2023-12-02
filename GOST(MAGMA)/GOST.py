8ed11e0587f675e720dd8ce3ceb59ac5cd1f41b8
root <92035508+AjayBadrinath@users.noreply.github.com>
2023-12-03 04:03:57 +0530

    Added Classes to Wrap Functions

--- GOST(MAGMA)/GOST.py
+++ GOST(MAGMA)/GOST.py
@@ -1,9 +1,13 @@
 
 '''
                                         Implementation Of GOST - R- 3412-2015 (MAGMA)
-author: AjayBadrinath
-date :  03-12-23
-
+Author: AjayBadrinath
+Date :  03-12-23
+Vesion :1.1
+                Version Changelog:
+                    1.Wrapped functions with class for Code Modularity
+                    2.Comments Yet to be added...
+                    3.Yet To perform Unit Test.
 
 '''
 
@@ -15,7 +19,8 @@ date :  03-12-23
     2. Pad with Zeros.
     
 '''
-s_box=[
+class GOST_MAGMA:
+    s_box=[
 
 	[0xC, 0x4, 0x6, 0x2, 0xA, 0x5, 0xB, 0x9, 0xE, 0x8, 0xD, 0x7, 0x0, 0x3, 0xF, 0x1],
 	[0x6, 0x8, 0x2, 0x3, 0x9, 0xA, 0x5, 0xC, 0x1, 0xE, 0x4, 0x7, 0xB, 0xD, 0x0, 0xF],
@@ -25,68 +30,79 @@ s_box=[
 	[0x5, 0xD, 0xF, 0x6, 0x9, 0x2, 0xC, 0xA, 0xB, 0x7, 0x8, 0x1, 0x4, 0x3, 0xE, 0x0],
 	[0x8, 0xE, 0x2, 0x5, 0x6, 0x9, 0x1, 0xC, 0xF, 0x4, 0xB, 0x0, 0xD, 0xA, 0x3, 0x7],
 	[0x1, 0x7, 0xE, 0xD, 0x0, 0x5, 0x8, 0x3, 0x4, 0xF, 0xA, 0x6, 0x9, 0xC, 0xB, 0x2]
-]
+    ]
 
-
-#Key Schedule input Key is  256 bits .
-def KeySchedule(key):
-    k_split=[]
-    k_sequence=[]
-    MASK=0xffffffff
-    for i in  range(7,-1,-1):
-        k_split.append(hex(key>>(32*i)&MASK))
-    #print(k_split)
-    #order asc->asc->asc->desc
-    for i in range(8):
-        k_sequence.append([k_split[i],k_split[i],k_split[i],k_split[len(k_split)-i-1]])
-    return k_sequence
-
-def rol(x):
-    return ((x<<11)^(x>>(32-11)))&0xffffffff
-
-
-def g_function(x,k):
-    return rol(Transform((x+k)%2**32))
-
-def Transform(x):
-    z=0
-    #0xcafebabe-> extract bits from MSB to LSB 
-    for i in range(7,-1,-1):
-        z=z<<4
-        z^=s_box[i][x>>(4*i)&0xf]
-    return z
-
-
-def flatten(l:list):
-    l1=[]
-    #print(len(l[0]))
-    for i in range(len(l[0])):
-        for j in l:
-            l1.append(j[i])
+    def __init__(self,message,key):
+        self.x=message
+        self.k=key
+    def rol(self,x):
+        return ((x<<11)^(x>>(32-11)))&0xffffffff
+    def Transform(self,x):
+        z=0
+        #0xcafebabe-> extract bits from MSB to LSB 
+        for i in range(7,-1,-1):
+            z=z<<4
+            z^=self.s_box[i][x>>(4*i)&0xf]
+        return z
     
-    return l1
+    def g_function(self,x,k):
+        return self.rol(self.Transform((x+k)%2**32))
+    #Key Schedule input Key is  256 bits .
+    def KeySchedule(self):
+        k_split=[]
+        k_sequence=[]
+        MASK=0xffffffff
+        for i in  range(7,-1,-1):
+            k_split.append(hex(self.k>>(32*i)&MASK))
+        #print(k_split)
+        #order asc->asc->asc->desc
+        for i in range(8):
+            k_sequence.append([k_split[i],k_split[i],k_split[i],k_split[len(k_split)-i-1]])
+        return k_sequence
+    def flatten(self,l:list):
+        l1=[]
+        #print(len(l[0]))
+        for i in range(len(l[0])):
+            for j in l:
+                l1.append(j[i])
+        
+        return l1
+    
+    #a is a 64 bit plaintext, k is the initial 256-bit key
+    def encrypt(self):
+        key=self.flatten(self.KeySchedule())
+        Left=(self.x>>32)&0xffffffff
+        Right=(self.x&0xffffffff)
+        #print(hex(Left),hex(Right))
+        for i in range(31):
+            (Left,Right)= (Right,Left^self.g_function(Right,int(key[i],16)))
+        
+        return ((Left^self.g_function(Right,int(key[-1],16)))<<32 )^Right
 
-#a is a 64 bit plaintext, k is the initial 256-bit key
-def encrypt(a,k):
-    key=flatten(KeySchedule(k))
-    Left=(a>>32)&0xffffffff
-    Right=(a&0xffffffff)
-    print(hex(Left),hex(Right))
-    for i in range(31):
-       (Left,Right)= (Right,Left^g_function(Right,int(key[i],16)))
-       
-    return ((Left^g_function(Right,int(key[-1],16)))<<32 )^Right
-
-def decrypt(c,k):
-    key=flatten(KeySchedule(k))
-    Left=(c>>32)&0xffffffff
-    Right=(c&0xffffffff)
-    print(hex(Left),hex(Right))
-    for i in range(31,0,-1):
-       (Left,Right)= (Right,Left^g_function(Right,int(key[i],16)))
-    return ((Left^g_function(Right,int(key[0],16)))<<32 )^Right
 
 
+    def decrypt(self):
+        key=self.flatten(self.KeySchedule())
+        Left=(self.x>>32)&0xffffffff
+        Right=(self.x&0xffffffff)
+        #print(hex(Left),hex(Right))
+        for i in range(31,0,-1):
+            (Left,Right)= (Right,Left^self.g_function(Right,int(key[i],16)))
+        return ((Left^self.g_function(Right,int(key[0],16)))<<32 )^Right
+
+
+
+
+
+
+
+
+
+
+s=GOST_MAGMA(0x4ee901e5c2d8ca3d,0xffeeddccbbaa99887766554433221100f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff)
+print(hex(s.decrypt()))
+
+'''
 
 p=Transform(int("fdb97531",16))
 #print(hex(p))
@@ -106,3 +122,4 @@ def convert_binary(a:str):
     return binseq
     
 #print(convert_binary("a"))
+'''
