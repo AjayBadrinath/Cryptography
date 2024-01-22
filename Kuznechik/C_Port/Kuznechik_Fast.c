4842f9ebfbf6737dcf2db37db7339c228685fc5c
AjayBadrinath <92035508+AjayBadrinath@users.noreply.github.com>
2024-01-23 00:44:03 +0530

    Портированный кузнечик в с

--- Kuznechik/C_Port/Kuznechik_Fast.c
+++ Kuznechik/C_Port/Kuznechik_Fast.c
@@ -4,13 +4,33 @@
 
 Author: AjayBadrinath
 Date :  18-01-24
-Version :1.0
-                Version Changelog: (27-12-23)
-                    1.Porting  From Python ...
-                    2.Extend Support For Arbitrary Prescision Numbers(gmp)
+Version :1.2
+                Version Changelog: (23-01-24)
+                    1.Ported  From Python ...     (_/)
+                    2.Extended Support For Arbitrary Prescision Numbers(gmp).(_/)
+                    3.Ported comments.
+                    4.Parallize operation with openmpi.(X)
 
 
+C Header Implementing the Kuznechik Cipher . This is a cipher Proposed by the Russian Security Standard GOST.
+    
+    The Technical Specifications of this cipher are in the pdf archive in Russian :
+    https://web.archive.org/web/20150924113434/http://tc26.ru/standard/gost/GOST_R_3412-2015.pdf
+    
+    Compared to the previous implementation of the GOST(MAGMA) Cipher which is of 64 bit block,
+    
+    Kuznechik is a 128 bit block cipher unlike its cousin MAGMA it is rather based on SP Network rather than the 
+    Fiestel System .
 
+    Note: Reading the source and understanding the same WILL Require Some Elementary Knowledge of Galois Field Arithmetic + Number Theory . 
+    
+    Some transformations are done using the same Pls Refer:
+    
+        1.https://math.stackexchange.com/questions/245621/arithmetic-operations-in-galois-field
+        2.https://en.wikipedia.org/wiki/Kuznyechik
+        3.The Russian Spec itself
+        4.https://stackoverflow.com/questions/13202758/multiplying-two-polynomials
+        5.With Support from gmp and openmpi
 
 
 */
@@ -20,6 +40,28 @@ Version :1.0
 #include <stdlib.h>
 #include <gmp.h>
 #include <assert.h>
+/**
+ * @brief 
+ * Constants:
+           
+            
+            Since this cipher is designed on the lines of Being a SP Network Cipher we have these s-boxes / permute-boxes.
+           
+            pi     : Used as non-linear bijective transformation    
+            
+            pi_inv : Used as non-linear bijective transformation
+            
+            Note   : Mathematically Speaking the inv (fn(x))->x 
+                So pi_inv(pi(x))=>x So the pi_inv can safely be called as pi inverse (inv_p-box)
+            
+            _map   : Iterator Array For the sake of conveinience used for Linear Transformation Constants (c)*nabla(x)
+            
+            MASK_32: This Acts as MASK as the name suggests to filter bits (32*4-bit )and prevent overflow 
+                    ik the name is misleading as it was named after 32 * hex not 32 bits so 128 bits(32*4)
+    
+ * 
+ */
+
 int  pi[]={
     252, 238, 221, 17, 207, 110, 49, 22, 251, 196, 250, 218, 35, 197, 4, 77, 233,
 119, 240, 219, 147, 46, 153, 186, 23, 54, 241, 187, 20, 205, 95, 193, 249, 24, 101,
@@ -36,6 +78,7 @@ int  pi[]={
 97, 32, 113, 103, 164, 45, 43, 9, 91, 203, 155, 37, 208, 190, 229, 108, 82, 89, 166,
 116, 210, 230, 244, 180, 192, 209, 102, 175, 194, 57, 75, 99, 182
 };
+
 int pi_inv[]={
     165, 45, 50, 143, 14, 48, 56, 192, 84, 230, 158,
     57, 85, 126, 82, 145, 100, 3, 87, 90, 28, 96,
@@ -73,6 +116,24 @@ int pi_inv[]={
  */
 void linear_Transformation(mpz_t ,mpz_t );
 void L_Transformation(mpz_t ,mpz_t );
+
+
+/**
+ * @brief 
+ *          
+ *          Tag:[Used in Encryption]
+            Function to Apply pi(p-box )to the input in the domain (V(128)->V(128)).
+
+            Parameters:
+                x: 128 bit input to Be substituted with pi
+            Returns   :
+                pi(x) with each nibble .
+
+ * 
+ * @param res 
+ * @param x 
+ */
+
 void S_Transformation(mpz_t res,mpz_t x){
     mpz_t s,tmp,tmp2,x_tmp;
     mpz_init(s);
@@ -108,7 +169,18 @@ void S_Transformation(mpz_t res,mpz_t x){
     mpz_clear(mask_8);
     mpz_clear(x_tmp);
 }
+/**
+ * @brief 
+ *          Tag:[ Used in Decryption ]
+            Function to Apply pi_inv(p_inv-box )to the input in the domain (V(128)->V(128)). 
 
+            Parameters:
+                x: 128 bit input to Be substituted with pi_inv
+            Returns   :
+                pi_inv(x) with each nibble .
+ * @param res 
+ * @param x 
+ */
 void S_Inv_Transformation(mpz_t res,mpz_t x){
     mpz_t s,tmp,tmp2,x_tmp;
     mpz_init(s);
@@ -145,6 +217,20 @@ void S_Inv_Transformation(mpz_t res,mpz_t x){
     mpz_clear(x_tmp);
 }
 
+/**
+ * @brief 
+ *          Tag:[Misc]
+            This is a helper function for Mod_Reduction. Computes the highest degree for a given polynomial:
+            eg:11010 -> 4
+
+            Parameters:
+                x: A binary Number / Polynomial Representation 
+            Returns:
+                Highest Degree for the given polynomial
+ * 
+ * @param x 
+ * @return int 
+ */
 
 int deg_Poly_V_128(mpz_t x){
     int deg=0;
@@ -159,6 +245,21 @@ int deg_Poly_V_128(mpz_t x){
     return deg;
 
 }
+
+/**
+ * @brief 
+ *          Tag: [Used in Encryption]
+            Function to perform R Transformation in the domain(V(128)->V(128))
+            
+            Parameters:
+                 x: 128 bit output from applying S-box transformation
+            
+            Returns:
+                 L(x(15)...x(0))||x(15)....x(1) -> 128 bit
+ * 
+ * @param res 
+ * @param x 
+ */
 void R_Transformation(mpz_t res,mpz_t x){
     mpz_t _res_lin_,_shift_r_120,_shr_8,_xor_,result,_x;
     mpz_init(_res_lin_);
@@ -182,6 +283,73 @@ void R_Transformation(mpz_t res,mpz_t x){
     mpz_clear(_xor_);
 
 }
+
+/**
+ * @brief 
+ *          Tag:[ Used in Decryption ]
+            Function to perform the inverse of R_Transformation in the domain(V(128)->V(128)) 
+
+            Parameters:
+                x: 128 bit input from L_Transformation_inverse function 
+            Returns   :
+                x(14)||x(13)...||x(0)||L(x(14)||x(13)||...||x(0)||x(15)) -> 128 bit 
+
+ * 
+ * @param res 
+ * @param x 
+ */
+
+void R_Transformation_inverse(mpz_t res,mpz_t x){
+    mpz_t _res_lin_,_shift_r_120,_shl_8,_xor_,result,_x,mask,_and;
+    mpz_init(_res_lin_);
+    mpz_init(result);
+    mpz_init(mask);
+    mpz_init(_and);
+    mpz_init(_x);
+    mpz_set(_x,x);
+    mpz_init(_shift_r_120);
+    mpz_set_str(mask,"ff",16);
+    mpz_init(_shl_8);
+    mpz_init(_xor_);
+    mpz_mul_2exp(_shl_8,_x,8);
+    mpz_tdiv_q_2exp(_shift_r_120,_x,120);
+    mpz_and(_and,_shift_r_120,mask);
+    mpz_xor(_xor_,_and,_shl_8);
+    linear_Transformation(_res_lin_,_xor_);
+    mpz_xor(result,_res_lin_,_shl_8);
+    //gmp_printf("resutltt: %Zx\n", result);
+    mpz_set(res,result);
+    mpz_clear(_x);
+    mpz_clear(_and);
+    mpz_clear(result);
+    mpz_clear(_res_lin_);
+    mpz_clear(mask);
+    mpz_clear(_shift_r_120);
+    mpz_clear(_shl_8);
+    mpz_clear(_xor_);
+
+}
+
+/**
+ * @brief 
+ * 
+ *          Tag:[Misc.]
+            The Delta Function Defined in the GOST Spec .
+            Utility Function to Multiply two Polynomials in binary to put it simply.
+            Essentially applies (x<< degree y)+(x<<degree  y)...where + refers to xor operation
+            Parameters:
+                x: Binary Polynomial 
+                y: Binary Polynomial
+            Returns:
+                x*y -> Product of two polynomials.
+
+
+ * 
+ * @param res 
+ * @param x 
+ * @param y 
+ */
+
 
 void Multiply_Poly_V_128(mpz_t res,mpz_t x ,mpz_t y){
     mpz_t c,lsh_deg,tmp,ones,_x,_y,_res;
@@ -225,6 +393,27 @@ void Multiply_Poly_V_128(mpz_t res,mpz_t x ,mpz_t y){
 
 
 }
+
+/**
+ * @brief 
+ *          Tag:[Misc]
+            Function to compute Mod for a given binary Polynomial in the field of GF(2) F->{0,1}
+            Essentially iterate till deg(z) <deg(mod):while performing (m<<diff) whrere diff is the 
+            difference between the highest power of z and m itself
+
+            Parameters:
+                x:A binary polynomial to be reduced.
+                m:Modulus binary Number .(For this Cipher We use )
+                GF(2)[x]->p(x)=x**8+x**7+x**6+x+1 (111000011) as mod
+
+            Returns:
+                z mod m
+ * 
+ * @param res 
+ * @param x 
+ * @param m 
+ */
+
 void Mod_Poly_Reduction(mpz_t res,mpz_t x,mpz_t m){
     mpz_t z,tmp;
     mpz_init(z);
@@ -249,6 +438,21 @@ void Mod_Poly_Reduction(mpz_t res,mpz_t x,mpz_t m){
     mpz_clear(tmp);
     mpz_clear(z);
 }
+
+/**
+ * @brief 
+ * 
+ *          Tag:[Used in Encryption][Used in Decryption]
+            Transformation proposed in the GOST Specification with Mapping Domain : V(8) ->V(8) 
+            Apply nabla(_map[iter]*delta(x))
+            Parameter:
+                x: input to transform
+            Returns:
+                LinearTransformation(x(15)||...||x(0))
+ * 
+ * @param res 
+ * @param x 
+ */
 
 void linear_Transformation(mpz_t res,mpz_t x){
     int _map_[]={
@@ -291,6 +495,22 @@ void linear_Transformation(mpz_t res,mpz_t x){
 
 }
 
+/**
+ * @brief 
+ *  
+ *          Tag:[Used in Key Schedule]
+            Utility Function Explicitly Written for sake of readablity .
+            Computes the RoundConstant c by applying L_Transformation
+            Parameters:
+                round_no: Number {0...9}
+            Returns:
+                L_Transformation(round_no) -> 256 bit
+ * 
+ * @param res 
+ * @param round_no 
+ */
+
+
 void Round_constant(mpz_t res,int round_no){
     mpz_t rno,result;
     mpz_init(rno);
@@ -303,6 +523,22 @@ void Round_constant(mpz_t res,int round_no){
     mpz_clear(result);
     mpz_clear(rno);
 }
+
+/**
+ * @brief 
+ *          Tag: [ Used in Encryption ]
+            Another Native Function to Kuznechik cipher to Apply R_Transformation func 16 times with the same domain. 
+            
+            Parameters:
+                x: 128 bit input for which R_Transformation function has to be applied
+            
+            Returns:
+                R_Transformation(R_Transformation....(x)) iterated 16 times -> 128 bits 
+ * 
+ * @param res 
+ * @param x 
+ */
+
 void L_Transformation(mpz_t res,mpz_t x){
     mpz_t tmp;
     mpz_init(tmp);
@@ -314,25 +550,55 @@ void L_Transformation(mpz_t res,mpz_t x){
     mpz_clear(tmp);
 
 }
+
+/**
+ * @brief 
+ *      
+ *          Tag: [ Used in Decryption ]
+            Native function that Applies the inverse of the L_Transformation by implicitly calling the R_Transformation_inverse function. Same domain Mapping.
+            
+            Parameters:
+                x: 128 bit input for which R_Transformation_inverse function has to be applied
+            Returns:
+                (R_Transformation_inverse(R_Transformation_inverse(....(x))) iterated 16 times) -> 128 bits 
+                
+ * 
+ * @param res 
+ * @param x 
+ */
+
 void L_Transformation_inverse(mpz_t res,mpz_t x){
     mpz_t tmp;
     mpz_init(tmp);
     mpz_set(tmp,x);
     for (int i=0;i<16;i++){
-       // R_Transformation_inverse(tmp,tmp);
+        R_Transformation_inverse(tmp,tmp);
 
     }
     mpz_set(res,tmp);
     mpz_clear(tmp);
 
 }
-int * p(int a,int b){
-    int * a1=malloc(sizeof(int)*2);
-    int*tmp=a1;
-    *(a1)=a;
-    *(a1+1)=b;
-    return a1;
-}
+
+/**
+ * @brief 
+ *          Tag:[Used in Key Schedule]
+            Native function for Key Generation or Key Schedule Generation . 
+            If you notice Interstingly this cipher follows a pattern of Fiestel Network for Key Schedule.
+            Parameters:
+                Key1:256 bit keys (Apply Combinations of Transformations to this )
+                Key2:256 bit keys  (Nothing done here . Just return the same key as is )
+                Round_Constant: Passed implicitly from the Round_constant function defined.
+            Returns: 
+                Array(L(S(roundconst^k1)),k2)
+ *       
+ * 
+ * @param round_const 
+ * @param k1 
+ * @param k2 
+ * @return mpz_t* 
+ */
+
 mpz_t*  F_Transformation(mpz_t round_const,mpz_t k1,mpz_t k2){
     mpz_t * arr=(mpz_t*)malloc(sizeof(mpz_t)*2);
     //mpz_t* temp=arr;
@@ -369,6 +635,24 @@ mpz_t*  F_Transformation(mpz_t round_const,mpz_t k1,mpz_t k2){
 
     return arr;
 }
+/**
+ * @brief 
+ * 
+ *          This is the Function to perform Key Deployment from the initial key.
+            This Key deployment algorithm uses Fiestel system to Get Separate Keys for each Round/iteration.
+            initial keys:k>>128,k&mask_32
+            Each set of keys obtained from each iteration are applied the F_Transformation.
+            Every 8th iteration keys thus obtained is added to key_list.
+
+            Parameters:
+                Key:256 Pseudorandomly generated Key 
+            Returns: 
+                KeySchedule Array of Size 10
+ * 
+ * @param key 
+ * @return mpz_t* 
+ */
+
 mpz_t* Key_Schedule(mpz_t key){
     printf("IN KEY_SCHED");
     mpz_t * key_arr=malloc(sizeof(mpz_t)*10);
@@ -453,6 +737,23 @@ mpz_t* Key_Schedule(mpz_t key){
     
     return key_arr;
 }
+
+/**
+ * @brief 
+ * 
+ *          Function To Encrypt Bloc Using Kuznechik Cipher .
+            Uses all the function marked as [Used in Encryption] tag in the docstring.
+            Parameters:
+                None
+                Implicitly uses the KeySchedule array for each round and apply L_Transformation(S(round_key^msg))
+            Returns:
+                128 bit Encrypted Cipher block
+ * 
+ * @param res 
+ * @param message 
+ * @param key 
+ */
+
 void encrypt(mpz_t res,mpz_t message,mpz_t key){
     mpz_t * key_arr_=malloc(sizeof(mpz_t)*10);
     printf("IN ENC");
@@ -480,9 +781,7 @@ void encrypt(mpz_t res,mpz_t message,mpz_t key){
     }
     mpz_xor(_res,msg,key_arr_[9]);
     mpz_set(res,_res);
-    for(int i=0;i<10;i++){
-        //mpz_clear(key_arr_[i]);
-    }
+    
     mpz_clear(_res);
     mpz_clear(_xor);
     mpz_clear(_key);
@@ -490,6 +789,62 @@ void encrypt(mpz_t res,mpz_t message,mpz_t key){
     mpz_clear(_lsx_);
     mpz_clear(msg);
 }
+
+
+/**
+ * @brief 
+ *          Function To Decrypt Block Using Kuznechik Cipher .
+            Uses all the function marked as [Used in Decryption] tag in the docstring.
+            Parameters:
+                None
+                Implicitly uses the KeySchedule array for each round and apply (S_inv(L_Transformation_inverse(round_key^cipher)))
+            Returns:
+                128 bit Decrypted Cipher block
+ * 
+ * @param res 
+ * @param cipher 
+ * @param key 
+ */
+void decrypt(mpz_t res,mpz_t cipher,mpz_t key){
+    mpz_t * key_arr_=malloc(sizeof(mpz_t)*10);
+    printf("IN DEC");
+    mpz_t msg,_xor,_Sx_,_lsx_,_key,_res;
+    for(int i=0;i<10;i++){
+        mpz_init(key_arr_[i]);
+    }
+    mpz_init(_xor);
+    mpz_init(_Sx_);
+    mpz_init(_res);
+    mpz_init(_lsx_);
+    mpz_init(_key);
+    mpz_init(msg);
+    mpz_set(msg,cipher);
+
+    mpz_set(_key,key);
+    key_arr_=Key_Schedule(_key);
+    for (int i=9;i>0;i--){
+         mpz_xor(_xor,key_arr_[i],msg);
+         L_Transformation_inverse(_lsx_,_xor);
+         S_Inv_Transformation(_Sx_,_lsx_);
+         mpz_set(msg,_Sx_);
+
+
+
+    }
+    mpz_xor(_res,msg,key_arr_[0]);
+    mpz_set(res,_res);
+    
+    mpz_clear(_res);
+    mpz_clear(_xor);
+    mpz_clear(_key);
+    mpz_clear(_Sx_);
+    mpz_clear(_lsx_);
+    mpz_clear(msg);
+
+
+}
+
+
 int main(){
 //printf("%lx",S_Transformation(0xffeeddccbbaa99881122334455667700)>>32);
 mpz_t test,msg,res,key,x,m;
@@ -503,12 +858,14 @@ mpz_set_str(x,"10101",2);
 mpz_set_str(m,"1100",2);
 mpz_init(msg);
 mpz_set_str(key,"8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef",16);
-mpz_set_str(msg,"1122334455667700ffeeddccbbaa9988",16);
+mpz_set_str(msg,"7f679d90bebc24305a468d42b9d4edcd",16);
 //Round_constant(res,5);
 
 //mpz_set_ui(res,10);
 mpz_set_str(test,"f9eae5f29b2815e31f11ac5d9c29fb01",16);
-encrypt(res,msg,key);
+//encrypt(res,msg,key);
+decrypt(res,msg,key);
+//R_Transformation_inverse(res,test);
 //Key_Schedule(key);
 //L_Transformation(res,test);
 //(x^4 + x^2 + 1) * (x^3 + x^2)
