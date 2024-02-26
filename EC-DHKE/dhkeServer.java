b86f808ea2e01191a66a0102c53521bcb2c153fc
AjayBadrinath <92035508+AjayBadrinath@users.noreply.github.com>
2024-02-27 00:12:51 +0530

    Commit Patches

--- EC-DHKE/dhkeServer.java
+++ EC-DHKE/dhkeServer.java
@@ -12,12 +12,36 @@ import java.io.PrintStream;
 import java.math.BigInteger;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-
+ class ModifiedTCPHeaderAuth{
+	ECPoint curveParam,generator,PublicKey;
+	ECField field;
+	
+	ModifiedTCPHeaderAuth(ECPoint curveParam,ECPoint generator,ECField field,ECPoint PublicKey){
+		this.curveParam=curveParam;
+		this.field=field;
+		this.generator=generator;
+		this.PublicKey=PublicKey;
+	}
+	List<Object> getHeader(){
+		List<Object> objects = new ArrayList<>();
+		objects.add("Chosen Curve Parameter a-:"+2+"b-:"+2+"\n");
+		objects.add(curveParam);
+		objects.add("Chosen Curve Generator Point:("+5+","+1+")\n");
+		objects.add(generator);
+		objects.add("Chosen Field:Z:"+17+"\n");
+		objects.add(field);
+		objects.add(PublicKey);
+		return objects;
+	}
+	
+}
 public class dhkeServer {
 	
 	public static void main(String[]args) {
-		ECPoint generator=new ECPoint(BigInteger.valueOf(0),BigInteger.valueOf(11));
+		ECPoint point;
 		ECPoint generator1=new ECPoint(BigInteger.valueOf(5),BigInteger.valueOf(1));
 		ECField field=new ECField(BigInteger.valueOf(17));
 		EllipticCurve curve=new EllipticCurve(BigInteger.valueOf(2),BigInteger.valueOf(2),field);
@@ -33,31 +57,64 @@ public class dhkeServer {
 			s1=serv.accept();
 			r2=new DataOutputStream(s1.getOutputStream());
 			r1=s1.getOutputStream();
-			
+			int i=5;
+			OutputStream os = s1.getOutputStream();
+			List<Object> objects = new ArrayList<>();
+			ECPoint curveParam=new ECPoint(BigInteger.valueOf(2),BigInteger.valueOf(2));
+			//List <ObjectOutputStream>s;
 			//Random s=new Random();
 			//s.nextInt(10);
 			//r2.writeUTF(null);
 			//p1=new PrintStream(s1.getOutputStream());
-			while(true) {
+			/*
+			objects.add("Chosen Curve Parameter a-:"+2+"b-:"+2+"\n");
+			ECPoint curveParam=new ECPoint(BigInteger.valueOf(2),BigInteger.valueOf(2));
+			objects.add(curveParam);
+			objects.add("Chosen Curve Generator Point:("+5+","+1+")\n");
+			objects.add(generator1);
+			objects.add("Chosen Field:Z:"+17+"\n");
+			objects.add(field);
+			ECPoint p=curve.ScalarMul(generator1, BigInteger.valueOf(17));
+			objects.add(p);
+			*/
+			ECPoint p=curve.ScalarMul(generator1, BigInteger.valueOf(17));
+			ModifiedTCPHeaderAuth auth=new ModifiedTCPHeaderAuth(curveParam,generator1,field,p);
+			
+			while(true) {    
+				List <Object> o=auth.getHeader();
+				ObjectOutputStream oos1 = new ObjectOutputStream(os);
+				oos1.writeObject(o);
+				
 				//r2.writeBytes();
-				r2.writeUTF("Chosen Curve Parameter a-:"+2+"b-:"+2);
-				r2.writeInt(2);
-				r2.writeInt(2);
-				r2.writeUTF("Chosen Curve Generator Point:("+5+","+1+")");
-				r2.writeInt(5);
-				r2.writeInt(1);
-				r2.writeUTF("Chosen Field:Z:"+17);
-				r2.writeInt(17);
+				/*
+				r2.writeUTF("Chosen Curve Parameter a-:"+2+"b-:"+2+"\n");
+				
+				
+				ECPoint curveParam=new ECPoint(BigInteger.valueOf(2),BigInteger.valueOf(2));
+				
+				oos1.writeObject(curveParam);
+				objects.add(curveParam);
+				
+				
+				r2.writeUTF("Chosen Curve Generator Point:("+5+","+1+")\n");
+				oos1.writeObject(generator1);
+				
+				
+				r2.writeUTF("Chosen Field:Z:"+17+"\n");
+				
+				oos1.writeObject(field);
+				
 				System.out.println("Chosen Private key:"+17);
+				///
+				
 				ECPoint p=curve.ScalarMul(generator1, BigInteger.valueOf(17));
-				OutputStream os = s1.getOutputStream();
-				ObjectOutputStream oos = new ObjectOutputStream(os);
-				oos.writeObject(p);
-				ObjectInputStream is=new ObjectInputStream(s1.getInputStream());
-				ECPoint p2=(ECPoint) is.readObject();
 				
 				
+				oos1.writeObject(p);
 				
+				i--;
+				
+				*/
 				
 			}
 		}catch(Exception e) {
--- EC-DHKE/dhkecCient.java
+++ EC-DHKE/dhkecCient.java
@@ -2,39 +2,45 @@ package ecc;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintStream;
 import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.List;
 
 import ecc.EllipticCurve;
 public class dhkecCient {
+	
 	public static void main (String []args) {
 		Socket s1;
 		DataInputStream r1,r2;
 		PrintStream p1;
 		int op;
-		ECPoint generator;
+		ECPoint curveParam;
 		ECPoint generator1;
 		ECField field;
 		EllipticCurve curve;
+		
 		try {
 			s1=new Socket(InetAddress.getLocalHost(),8080);
 			r1=new DataInputStream(s1.getInputStream());
-			byte[ ]tmp=new byte[256];
-			int i=2;
-			//r2=new BufferedReader(new InputStreamReader(s1.getInputStream()));
-			//p1=new PrintStream(s1.getOutputStream());
+			
+			int i=1;
+			InputStream is=s1.getInputStream();
+			
 			while(i!=0) {
-				//("Client :");
-				System.out.print(r1.readUTF());
-				generator=new ECPoint(BigInteger.valueOf(r1.readInt()),BigInteger.valueOf(r1.readInt()));
-				//System.out.println(new String(tmp));
-				op=r1.readInt();
+				ObjectInputStream p=new ObjectInputStream(is);
+				Object tmp=p.readObject();
+				List<?> header=null;
+				header=(List<?>) tmp;
+				System.out.println(header.get(0));
+				
 				i--;
 				
-				//System.out.println("Server: "+op);
 				
 			}
 		}  catch (Exception e) {
