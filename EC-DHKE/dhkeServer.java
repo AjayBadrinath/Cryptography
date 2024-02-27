bc023d0fb7448d28a330ca55e4ab55233bd49c6c
AjayBadrinath <92035508+AjayBadrinath@users.noreply.github.com>
2024-02-27 16:41:24 +0530

    Added comments to dhke

--- EC-DHKE/dhkeServer.java
+++ EC-DHKE/dhkeServer.java
@@ -1,4 +1,18 @@
 package ecc;
+/*
+ * 											Elliptic Curve Diffie Hellman Key Exchange Protocol Client
+ * 
+ * @author : AjayBadrinath
+ * @date   : 26-02-2024
+ * 
+ * Version : 1.0.2
+ * 
+ * 					Version Changelog:
+ * 									 
+ * 									1. Fixed Byte Sequence Error
+ * 									2. Authentication and Establishment of shared Key Done !
+ * 
+ * */
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -8,7 +22,16 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
- class ModifiedTCPHeaderAuth{
+/**
+ * Class To Provide TCP Header For Key Exchange . Comment Out String if not needed.....
+ * Header Format(Not Including String):
+ * 			1.curveParameter
+ * 			2.Primitive Point Generator
+ * 			3.Field
+ * 			4.Computed Public Key aP=Pub_key_a
+ * DO NOT PARSE IN WRONG ORDER 
+ */
+ class ModifiedTCPHeaderAuth {
 	ECPoint curveParam,generator,PublicKey;
 	ECField field;
 	
@@ -18,6 +41,10 @@ import java.util.List;
 		this.generator=generator;
 		this.PublicKey=PublicKey;
 	}
+	/**
+	 * Function to set header parameters to send via tcpstream.
+	 * @return List <Object>
+	 */
 	List<Object> getHeader(){
 		List<Object> objects = new ArrayList<>();
 		objects.add("Chosen Curve Parameter a-:"+2+"b-:"+2+"\n");
@@ -31,20 +58,39 @@ import java.util.List;
 	}
 	
 }
+/**
+ * Main Class Server.
+ */
 public class dhkeServer {
-	
+	/**
+	 * Entry Point for the Server.
+	 * @param None
+	 */
 	public static void main(String[]args) {
+		/**
+		 * Can Choose any Curve Parameter .. Can Be Random too.
+		 * 
+		 * 
+		 */
 		ECPoint point;
 		ECPoint generator1=new ECPoint(BigInteger.valueOf(5),BigInteger.valueOf(1));
 		ECField field=new ECField(BigInteger.valueOf(17));
 		EllipticCurve curve=new EllipticCurve(BigInteger.valueOf(2),BigInteger.valueOf(2),field);
 		Socket s1;
 		ServerSocket serv;
+
+		/**
+		 * Open Server Connection .
+		 */
 		try {
 			serv=new ServerSocket(8080);
-			
 			s1=serv.accept();
 			int i=1;
+			/**
+			 * 
+			 * Opening Output Stream / InputStream for Key Exchange.
+			 * 
+			 */
 			OutputStream os = s1.getOutputStream();
 			InputStream is=s1.getInputStream();
 			ECPoint curveParam=new ECPoint(BigInteger.valueOf(2),BigInteger.valueOf(2));
@@ -54,20 +100,40 @@ public class dhkeServer {
 			ModifiedTCPHeaderAuth auth=new ModifiedTCPHeaderAuth(curveParam,generator1,field,p);
 			
 			while(i!=0) {  
-				
+				/**
+				 * Generate Header 
+				 */
 				List <Object> o=auth.getHeader();
 				ObjectOutputStream oos1 = new ObjectOutputStream(os);
+				/**
+				 * 
+				 * Send header to tcp socket.
+				 * 
+				 */
 				oos1.writeObject(o);
-				
+				/**
+				 * 
+				 * Recieve Client Public Key bP=Client_Pub.
+				 * 
+				 */
 				ObjectInputStream p3=new ObjectInputStream(is);
 				List<?>B=(List<?>) p3.readObject();
-				
+
+				/**
+				 * Establish Shared key(Shared Secret) .. Must be the same at this point on the client and server 
+				 * Proof Of correctness in readme...
+				 * 
+				 */
 				ECPoint B_Pub=(ECPoint) B.get(0);
+
+
 				ECPoint shared_key=curve.ScalarMul(B_Pub,privKey);
 				System.out.println("Shared Key:"+shared_key.x+" "+shared_key.y);
 				
 				i--;
-				
+				/**
+				 * Close Stream after handshake.
+				 */
 				oos1.close();
 				p3.close();
 			}
--- EC-DHKE/dhkecClient.java
+++ EC-DHKE/dhkecClient.java
@@ -5,12 +5,12 @@ package ecc;
  * @author : AjayBadrinath
  * @date   : 26-02-2024
  * 
- * Version : 1.0.2
+ * Version : 1.0.3
  * 
  * 					Version Changelog:
- * 									1. Yet to add comments 
- * 									2. Fixed Byte Sequence Error
- * 									3. Authentication and Establishment of shared Key Done !
+ * 					
+ * 									1. Fixed Byte Sequence Error
+ * 									2. Authentication and Establishment of shared Key Done !
  * 
  * */
 import java.io.DataInputStream;
@@ -24,13 +24,21 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * 
+ * Class to Send header with Client Public Key  (bP)->Client_Pub to server .
+ */
 class ModifiedTCPHeaderAuthClient{
 	ECPoint PublicKey;
 	ModifiedTCPHeaderAuthClient(ECPoint PublicKey){
 		this.PublicKey=PublicKey;
 		
+
 	}
+	/**
+	 * Class to generate another header payload from client .. Now only the public key from client is needed.
+	 * @return List <Object> -> List <ECPoint>
+	 */
 	List<Object> getHeader(){
 		List<Object> objects = new ArrayList<>();
 		objects.add(PublicKey);
@@ -38,9 +46,24 @@ class ModifiedTCPHeaderAuthClient{
 	}
 	
 }
+/**
+ * 
+ * Client Class 
+ * 
+ */
 public class dhkecCient {
-	
+	/**
+	 * Class Entry point 
+	 * @param None
+	 */
 	public static void main (String []args) {
+
+		/**
+		 * 
+		 * Recieve parameters from Server.
+		 * Construct Elliptic Curve using Server parameters.
+		 * 
+		 */
 		Socket s1;
 		ECPoint curveParam;
 		ECPoint generator;
@@ -48,30 +71,49 @@ public class dhkecCient {
 		EllipticCurve curve;
 		
 		try {
+			/**
+			 * 
+			 * Open Socket for Communication with server .
+			 * 
+			 */
 			s1=new Socket(InetAddress.getLocalHost(),8080);
 			OutputStream os = s1.getOutputStream();
 			int i=1;
 			InputStream is=s1.getInputStream();
 			
 			while(i!=0) {
+				/**
+				 * Open InputStream for getting server Parameters for 
+				 */
 				ObjectInputStream p=new ObjectInputStream(is);
 				Object tmp=p.readObject();
 				List<?> header=null;
 				header=(List<?>) tmp;
-				
+				/**
+				 * Generate similar Curve and perform computation.
+				 */
 				curveParam=(ECPoint )header.get(1);
 				field=(ECField )header.get(5);
 				generator=(ECPoint )header.get(3);
 				curve=new EllipticCurve(curveParam.x,curveParam.y,field);
-				
+				/**
+				 * Generating Public Key With Common Primitive and priv key.
+				 */
 				BigInteger privKey= BigInteger.valueOf(11);
 				ECPoint p11=curve.ScalarMul(generator,privKey);
-				
+				/**
+				 * Creating reply header with client public Key A
+				 * 
+				 */
 				ModifiedTCPHeaderAuthClient reply=new ModifiedTCPHeaderAuthClient(p11);
 				System.out.println(p11.x+""+p11.y);
 				ObjectOutputStream oos1 = new ObjectOutputStream(os);
 				oos1.writeObject(reply.getHeader());
 				ECPoint A_Pub=(ECPoint) header.get(6);
+				/**
+				 * Computing Shared key must be same .
+				 * Now One can use this key to Perform Additional Computation over aes
+				 */
 				ECPoint shared_key=curve.ScalarMul(A_Pub,privKey);
 				System.out.println("Shared Key:"+shared_key.x+" "+shared_key.y);
 				
